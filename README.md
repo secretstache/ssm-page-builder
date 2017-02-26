9c89e274483b0acf483fdd8fbd3fec37e9ef10e6
Rich Staats <rich@secretstache.com>
2017-02-26 13:39:18 -0700

    refactoring

--- README.md
+++ README.md
@@ -7,9 +7,9 @@ This repo is a drop-in for adding a WordPress page builder using ACF.
 1. Install Advanced Custom Fields Pro (required)
 2. Install SSM Core (temporarily required)
 3. Drop `acf-json` into the root of your theme.
-4. Drop `src/lib/SSMPB` into your theme
-5. Update the constants in `/src/ssmpb.php`
-6. Require `SSMPB/inc/page-builder/sections.php` in your theme templates.
+4. Drop `SSMPB` into your theme
+5. Update the constants in `SSMPB/ssmpb.php`
+6. Require `SSMPB/ssmpb.php` in `functions.php` or similar.
 
 
 ### Credits
--- src/lib/SSMPB/ssmpb.php
+++ src/lib/SSMPB/ssmpb.php
@@ -4,21 +4,24 @@ namespace SSMPB;
 define( 'SSMPB_VERSION', '0.1');
 define( 'SSMPB_DIR', trailingslashit( get_stylesheet_directory() . '/src/lib/SSMPB' ) );
 define( 'SSMPB_DIR_URI', trailingslashit( get_stylesheet_directory_uri() . '/src/lib/SSMPB' ) );
-define( 'SSMPB_INC', trailingslashit( SSMPB_DIR . 'inc' ) );
-define( 'SSMPB_INC_URI', trailingslashit( SSMPB_DIR_URI . 'inc' ) );
 
 require( SSMPB_DIR . 'functions/helpers.php');
 
 add_action('admin_enqueue_scripts', __NAMESPACE__ . '\\css');
-
-
+/**
+ * Admin CSS
+ * @since 1.0.0
+ */
 function css() {
     wp_register_style( 'ssmpb-styles', SSMPB_DIR_URI . 'styles/ssmpb.css', '', SSMPB_VERSION );
     wp_enqueue_style( 'ssmpb-styles' );
 }
 
 add_action('admin_enqueue_scripts', __NAMESPACE__ . '\\js');
-
+/**
+ * Admin JS
+ * @since 1.0.0
+ */
 function js() {
     wp_register_script( 'ssmpb-scripts', SSMPB_DIR_URI . 'scripts/ssmpb.js', array('jquery'), SSMPB_VERSION );
     wp_enqueue_script( 'ssmpb-scripts' );
